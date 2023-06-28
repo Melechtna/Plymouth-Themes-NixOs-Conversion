97fafb55a2ea2cc2e9acfdc04cd2e251b58f9024
Melechtna <melechtna@gmail.com>
2023-06-28 11:14:48 -0500

    fetchurl it is

--- PlyTheme.nix
+++ PlyTheme.nix
@@ -1,18 +1,16 @@
 { stdenv, fetchFromGitHub }:
-{
-   PlyTheme = stdenv.mkDerivation rec {
+  stdenv.mkDerivation rec {
     pname = "plymouth-vinyl-theme";
     version = "1.0";
     dontBuild = true;
-    installPhase = ''
-    mkdir -p ${out}/share/plymouth/themes/
-    cp -aR ${src} ${out}/share/plymouth/themes/
-    '';
-    src = fetchFromGitHub {
+        src = fetchFromGitHub {
       owner = "Melechtna";
       repo = "Plymouth-Themes-NixOs-Conversion";
       rev = "v${version}";
       sha256 = "8992f0a19c497e6dc17e6bdb22c7f3e260f342582071ff09f01c88aecdda4e56";
     };
-  };
-}
+    installPhase = ''
+    mkdir -p $out/share/plymouth/themes/
+    mv * $out/share/plymouth/themes/
+    '';
+  }
