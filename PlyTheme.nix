06985cc9e64e32b42c3d50526b08dd1474e17b21
Melechtna <melechtna@gmail.com>
2023-06-28 13:22:17 -0500

    Fixed naming for animation and updated nix

--- PlyTheme.nix
+++ PlyTheme.nix
@@ -2,14 +2,21 @@
   stdenv.mkDerivation rec {
     pname = "plymouth-vinyl-theme";
     version = "1.0";
-    dontBuild = true;
     src = fetchurl {
     url = "https://raw.githubusercontent.com/Melechtna/Plymouth-Themes-NixOs-Conversion/vinyl/vinyl.tar.gz";
-     sha256 = "abc90b1b26ae315df22e37b89ab04c1733df6c90bac1ca0bc612ab312636d6dc";
+    sha256 = "2df4ae288d5498ee6a9c343f34826d0f5ad8eebf1c1e3ac6f71a7ce454fb76ad";
   };
+
+    dontConfigure = true;
+    dontBuild = true;
+
     installPhase = ''
-    sed -i 's:\(^ImageDir=\)/usr:\1'"$out"':' vinyl.plymouth
-    mkdir -p $out/share/plymouth/themes/vinyl
-    mv * $out/share/plymouth/themes/vinyl
+      runHook preInstall
+
+      mkdir -p $out/share/plymouth/themes/
+      tar -xzvf $src -C $out/share/plymouth/themes/
+      substituteInPlace $out/share/plymouth/themes/vinyl/*.plymouth --replace '@IMAGES@' "$out/share/plymouth/themes/vinyl/images"
+
+      runHook postInstall
     '';
   }
