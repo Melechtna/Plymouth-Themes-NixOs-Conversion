5af28d17c5829963abfbe5806e44e5e72046ce11
Melechtna <melechtna@gmail.com>
2023-06-28 10:29:00 -0500

    Add sha256

--- PlyTheme.nix
+++ PlyTheme.nix
@@ -10,9 +10,9 @@
     '';
     src = fetchFromGitHub {
       owner = "Melechtna";
-      repo = "Plymouth-Themes-NixOs-Conversion/tree/vinyl";
+      repo = "Plymouth-Themes-NixOs-Conversion";
       rev = "v${version}";
-      sha256 = "dunno yet";
+      sha256 = "8992f0a19c497e6dc17e6bdb22c7f3e260f342582071ff09f01c88aecdda4e56";
     };
   };
 }
