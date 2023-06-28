ff072596d80b5e4b9caae309c8c290d1bfd26d39
Melechtna <melechtna@gmail.com>
2023-06-28 10:12:53 -0500

    Not sure why it chunked itself

--- PlyTheme.nix
+++ PlyTheme.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHubl }:
+{ stdenv, fetchFromGitHub }:
 {
    PlyTheme = stdenv.mkDerivation rec {
     pname = "plymouth-vinyl-theme";
@@ -6,16 +6,6 @@
     installPhase = ''
     mkdir -p ${out}/share/plymouth/themes/
     cp -aR ${src} ${out}/share/plymouth/themes/
-    echo "[Plymouth Theme]
-    Name=Vinyl
-    Description=My Little Pony bootsplash Vinyl Scratch DJ Pon3 version
-    ModuleName=script
-
-
-    [script]
-    ImageDir=/run/current-system/sw/share/plymouth/themes/vinyl
-    ScriptFile=/run/current-system/sw/share/plymouth/themes/vinyl/vinyl.script" > ${out}share/plymouth/themes/vinyl/vinyl.script
-    "
     '';
     src = fetchFromGitHub {
       owner = "Melechtna";
